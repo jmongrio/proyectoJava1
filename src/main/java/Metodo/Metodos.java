a2bb14f13c5af7c5590a50e1375bc118f6c62120
Jmongrio <jmongrio@hotmail.com>
2020-04-23 13:22:17 -0600

    Se agrega la clase tiquete

--- src/main/java/Metodo/Metodos.java
+++ src/main/java/Metodo/Metodos.java
@@ -4,73 +4,26 @@ import javax.swing.JOptionPane;
 
 public class Metodos 
 {
-    public void checkBoleto(int selecDestino, double costoBoleto, String destino)
-    {     
-        if(selecDestino == 1) //Nicaragua
-        {
-            costoBoleto = 95;
-            destino = "NCA";
-            
-        }
-        else
-        {
-            if(selecDestino == 2) //Panamá
-            {
-                costoBoleto = 100;
-                destino = "PNA";
-            
-            }//Fin if
-            
-        }//Fin if
-    }//Fin checkBoleto
-    
-    public double checkDescuento(int edad, double costoBoleto)
+    public boolean checkEspacios(String apellido1, String apellido2, String nombre, String pasaporte, int destino, int genero)
     {
-        double descuento = 0;
-
-        if(edad <= 14)
-        {
-            descuento = (costoBoleto - (costoBoleto * 0.10));
-        }
-        else
-        {
-            if(edad > 65)
-            {
-                descuento = (costoBoleto - (costoBoleto * 0.35));
-               
-            }//Fin if
-
-        }//Fin if
-        return descuento;
+        boolean continuar = true;
         
-    }//Fin checkDescuento
-    
-    
-    public void checkEspacios(String apellido1, String apellido2, String nombre, String pasaporte, int destino, int edad, int genero, int regPension)
-    {
         if((nombre == "") || (apellido1 == "") || (apellido2 == "") || (pasaporte == ""))
         {
             JOptionPane.showMessageDialog(null, "Debe llenar todos los espacios.");
-        
-        }//Fin if
-        
-        if((genero == 0) || (destino == 0) || ((edad > 65) && (regPension == 0)))
-        {
-            JOptionPane.showMessageDialog(null, "Debe seleccionar todas las opciones.");
-        }//Fin if
-        
-    }//Fin checkEspacios
-
-    public void checkCiudadanoOro(int edad, String ciudadanoOro)
-    {
-        if(edad >= 65)
-        {
-            ciudadanoOro = "Si";            
+            continuar = false;
         }
         else
         {
-            ciudadanoOro = "No";
-        }
-    }//Fin checkCiudadanoOro
-    
+            if((genero == 0) || (destino == 0) )
+            {
+                JOptionPane.showMessageDialog(null, "Debe seleccionar todas las opciones.");
+                continuar = false;
+            }//Fin if
+            
+        }//Fin if
+        return continuar;
+        
+    }//Fin checkEspacios
+      
 }//Fin Metodos
---
+++ src/main/java/Metodo/Tiquete.java
@@ -0,0 +1,71 @@
+package Metodo;
+
+public class Tiquete 
+{
+    //Formato de tiquete Destino,Asiento ejemplo(PNA00).
+    
+    //Calcula el costo del boleto.
+    public double costoBoleto(int selecDestino, int edad)
+    {   
+        double costoBoleto = 0;
+        //double boletoNeto = 0;
+        
+        if(selecDestino == 1) //Panamá.
+        {
+            costoBoleto = 100;
+            
+        }
+        else
+        {
+            if(selecDestino == 2)  //Nicaragua.
+            {
+                costoBoleto = 95;
+            
+            }//Fin if.
+            
+        }//Fin if.
+        
+        if(edad <= 14)
+        {
+            costoBoleto = (costoBoleto - (costoBoleto * 0.10));
+        }
+        else
+        {
+            if(edad > 65)
+            {
+                costoBoleto = (costoBoleto - (costoBoleto * 0.35));
+               
+            }//Fin if.
+
+        }//Fin if.
+        return costoBoleto;
+        
+    }//Fin costoBoleto.
+    
+    //Generación de tiquete.
+    public String generaTiquete(String destino, String asiento)
+    {     
+        String Destino;
+        String Tiquete = "";
+        
+        if(destino == "Nicaragua")
+        {
+            Destino = "NCA";
+            Tiquete = (Destino + asiento);
+
+        }
+        else
+        {
+            if(destino == "Panamá")
+            {
+                Destino = "PNA";
+                Tiquete = (Destino + (String.valueOf(asiento)));
+                
+            }//Fin If.
+            
+        }//Fin if.
+        return Tiquete;
+        
+    }//Fin generaTiquete.
+    
+}//Fin Tiquete.
