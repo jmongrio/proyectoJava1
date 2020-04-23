cd72915da3df2e1a7155474c8e6031ea455b6aaf
jmongrio <jmongrio@hotmail.com>
2020-04-23 10:43:00 -0600

    Clase main

---
+++ src/main/java/GUI/main.java
@@ -0,0 +1,12 @@
+package GUI;
+
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        UI ui = new UI();
+        
+        ui.setVisible(true);
+    }//Fin main
+    
+}//Fin main
